df287bba3f97dcb3609b4c67fc6b62714c5757e1
Shivang Mishra <githubshiv@gmail.com>
2025-01-29 14:23:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,7 +87,7 @@ PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/78dce13fe7648784e9260-846be6d7c1bed528c2.jpg"
 )
 PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
-STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
+STATS_IMG_URL = "https://graph.org/file/b822fd712d27e810d9b1c-e23ab483baa5832312.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
